0ebeaea6a49d3a6fdf2a5cd4c3b4bd945253362a
Evie Ebert <evelyn.ebert@gmail.com>
2017-11-03 10:57:20 -0400

    Add files via upload

---
+++ first-springboot-project/src/main/java/com/in28minutes/springboot/WelcomeController.java
@@ -0,0 +1,25 @@
+package com.in28minutes.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.in28minutes.service.WelcomeService;
+
+@RestController
+public class WelcomeController {
+
+	//Autowiring
+	@Autowired
+	private WelcomeService service;
+
+	@RequestMapping("/welcome")
+	public String welcome() {
+		return service.retrieveWelcomeMessage();
+	}
+
+
+
+}
+
